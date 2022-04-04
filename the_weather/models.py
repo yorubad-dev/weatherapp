19c2bd9ab6022562edacf478ab41981b7886af44
KingAnointing <adetoyeanointing15@gmail.com>
2022-04-04 07:47:36 +0100

    create city model

--- the_weather/models.py
+++ the_weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+
+    name = models.CharField(max_length=35)
+
+    class Meta:
+        verbose_name = "city"
+        verbose_name_plural = "cities"
+
+    def __str__(self):
+        return self.name
--- the_weather/views.py
+++ the_weather/views.py
@@ -7,3 +7,14 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=090ffecfae05341a427634840e247205'
 
     response = requests.get(format(url)).json()
+    
+
+    city_weather = {
+        'city': city,
+        'temperature': response['main']['temp'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
+        'humidity': response['main']['humidity'],
+        'pressure': response['main']['pressure'],
+        'wind': response['wind']['speed']
+    }
