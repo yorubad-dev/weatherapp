f5a642da3760fc13ced48b2292b6b76edc6c78b4
KingAnointing <adetoyeanointing15@gmail.com>
2022-04-04 07:37:38 +0100

    call and convert API to json

--- the_weather/views.py
+++ the_weather/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
-    pass
+    
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=090ffecfae05341a427634840e247205'
+
+    response = requests.get(format(url)).json()
