ce10875b0b6f4e33c184602b0fbf259cea64a6fb
KingAnointing <adetoyeanointing15@gmail.com>
2022-04-05 01:46:41 +0100

    added message and class to context and fixed bug

--- the_weather/views.py
+++ the_weather/views.py
@@ -9,7 +9,7 @@ from the_weather.models import City
 def index(request):
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=090ffecfae05341a427634840e247205'
-    err_msg = ''
+    err_message = ''
     message = ''
     message_class = ''
 
@@ -21,19 +21,19 @@ def index(request):
             if existing_city_count == 0:
                 response = requests.get(url.format(new_city)).json()    
                 if response['cod'] == 200:
-                    message = 'City Added Successfully'
+                    msg = 'City Added Successfully'
                     form.save()
                 else:
                     err_message = 'City weather record not available at moment'
             else:
                 err_message = 'City Already Exist'
 
-    if err_msg:
-        err_msg = err_message
-        message_class = 'is-danger'
-    else:
-        message = message
-        message_class = 'is-success'
+        if err_message:
+            message = err_message
+            message_class = 'is-danger'
+        else:
+            message = msg
+            message_class = 'is-success'
 
     form = CityForm()
     weather_data = []
@@ -57,6 +57,9 @@ def index(request):
 
     context = {
         'weather_data': weather_data,
-        'form': form
+        'form': form,
+        'message': message,
+        'message_class': message_class,
+
     }
     return render(request, 'the_weather/index.html', context)
